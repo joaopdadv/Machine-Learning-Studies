ae65ce49f97971cf2aad7bfcaadcb13786b246c0
joaopdadv <joaopdadv@gmail.com>
2024-09-26 10:34:16 -0300

    adds activation functions module

---
+++ modules/activation_functions.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class ReLU():
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        return np.maximum(0, x) # retorna 0 se x < 0, x se x >= 0
+
+    def __call__(self, x):
+        return self.forward(x)
