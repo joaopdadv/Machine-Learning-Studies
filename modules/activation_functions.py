60909312fc52cb3e02add1bca4b5a201a4d2b797
joaopdadv <joaopdadv@gmail.com>
2024-09-30 10:46:33 -0300

    jacobian

--- modules/activation_functions.py
+++ modules/activation_functions.py
@@ -4,8 +4,35 @@ class ReLU():
     def __init__(self):
         pass
 
-    def forward(self, x):
+    def forward(self, x): # x shape deve ser (1, din)
+        self.x = x
         return np.maximum(0, x) # retorna 0 se x < 0, x se x >= 0
+    
+    def backward(self):
+        din = dout = self.x.shape[1] # x.shape[1] é o din
+        jacobian = np.zeros((dout, din))
+        for i in range(din):
+            if self.x[0, i] >= 0:
+                jacobian[i, i] = 1
+
+    def __call__(self, x):
+        return self.forward(x)
+    
+    def load(self, path):
+        pass # Não faz nada
+
+    def save(self, path):
+        pass # Não faz nada
+
+class Softmax():
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        return np.exp(x) / np.exp(x).sum()
+    
+    def backward(self):
+        raise NotImplementedError("Softmax backward not implemented")
 
     def __call__(self, x):
         return self.forward(x)
--- modules/mlp.py
+++ modules/mlp.py
@@ -4,12 +4,23 @@ class MLP():
     def __init__(self, din, dout):
         self.W = np.random.randn(dout, din)
         self.b = np.random.randn(dout) # bias array tem que ter o mesmo tamanho que a saída
+        self.dout = dout
+        self.din = din
 
     def forward(self, x): # x shape deve ser (1, din)
         # x é um vetor de entrada
         # @ é multiplicação de matrizes no numpy
         # .T é a transposta   
+        self.x = x # guarda x para usar no backward
         return x @ self.W.T + self.b
+    
+    def backward(self):
+        jacobian_x = self.W # jacobian_x é a derivada da saída com relação a x
+        jacobian_b = np.eye(self.dout) # jacobian_b é a derivada da saída com relação a b
+        jacobian_w = np.zeros((self.dout, self.din * self.dout)) # jacobian_w é a derivada da saída com relação a W. é uma matriz 2D que é zerada no inicio
+        # Coloca os valores de jacobian_w, que são as entradas x, nas posições corretas 
+        for i in range(self.dout):
+            jacobian_w[i, i * self.din: (i+1) * self.din] = self.x
     
     def __call__(self, x):
         return self.forward(x)
@@ -32,6 +43,10 @@ class CompoundNN():
         for block in self.blocks:
             x = block.forward(x)
         return x
+    
+    def backward(self):
+        for block in self.blocks:
+            block.backward()
 
     def __call__(self, x):
         return self.forward(x)
