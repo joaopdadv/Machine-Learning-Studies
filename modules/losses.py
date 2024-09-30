f93321755276139bf43dcf5d22ca2d04e987e644
joaopdadv <joaopdadv@gmail.com>
2024-09-30 14:05:33 -0300

    gradient tests

---
+++ modules/losses.py
@@ -0,0 +1,15 @@
+class MSELoss():
+
+    def forward(self, pred, true):
+        return ((pred - true) ** 2).mean()
+    
+    def __call__(self, pred, true):
+        self.pred = pred
+        self.true = true
+        return self.forward(pred, true)
+    
+    def backward(self):
+        din = self.pred.shape[1]
+        jacobian = 2 * (self.pred - self.true) * 1 / din
+
+        return jacobian
