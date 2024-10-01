5c882da2d4031ceef8679642f89a184b589c819b
joaopdadv <joaopdadv@gmail.com>
2024-10-01 11:18:36 -0300

    NLLLoss

--- modules/losses.py
+++ modules/losses.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class MSELoss():
 
     def forward(self, pred, true):
@@ -11,5 +13,22 @@ class MSELoss():
     def backward(self):
         din = self.pred.shape[1]
         jacobian = 2 * (self.pred - self.true) * 1 / din
+
+        return jacobian
+    
+class NLLLoss():
+    
+    def forward(self, pred, true):
+        self.pred = pred
+        self.true = true
+        return - pred[0, true]
+    
+    def __call__(self, pred, true):
+        return self.forward(pred, true)
+    
+    def backward(self):
+        din = self.pred.shape[1]
+        jacobian = np.zeros((1, din))
+        jacobian[0, self.true] = -1
 
         return jacobian
